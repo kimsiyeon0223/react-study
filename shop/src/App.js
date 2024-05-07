2bd30d3b5ebce6597a2ad8b8a21069eda5312e49
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-05-07 17:30:04 +0900

    Card 컴포넌트 만들고 map 사용해보기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import { Button, Nav, Navbar, Container } from "react-bootstrap";
+import data from "./data";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <Button variant="primary">primary</Button>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -17,33 +20,11 @@ function App() {
       </Navbar>
 
       <div className="main-bg"></div>
-
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i + 1}></Card>;
+          })}
         </div>
       </div>
     </div>
@@ -51,3 +32,17 @@ function App() {
 }
 
 export default App;
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+        alt=""
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
+}
