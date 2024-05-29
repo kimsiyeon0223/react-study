4f9d9f0856523720f89ada818adbd8c6d3029145
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-05-29 09:55:44 +0900

    detail 페이지 수정

---
+++ shop/src/routes/Detail.js
@@ -0,0 +1,28 @@
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+  let { id } = useParams();
+  let finGoods = props.shoes.find(function (x) {
+    return x.id == id;
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{finGoods.title}</h4>
+          <p>{finGoods.content}</p>
+          <p>{finGoods.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Detail;
