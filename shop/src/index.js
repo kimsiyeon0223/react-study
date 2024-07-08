1560fa5d98d275cb756b8c459044bb0283298657
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-08 23:44:33 +0900

    redux 세팅

--- shop/src/index.js
+++ shop/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    {" "}
+    {/*자식 컴포넌트들은 store에 있는 state를 전부 사용할 수 있다.*/}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
 );
