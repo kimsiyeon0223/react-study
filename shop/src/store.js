f59131d58ec94331e37f86364a518900b1449ccd
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-09 23:54:22 +0900

    state등록하기

--- shop/src/store.js
+++ shop/src/store.js
@@ -1,5 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+let user = createSlice({
+  //useState역할, 슬라이스라고 부름
+  name: "user",
+  initialState: "kim",
+});
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    user: user.reducer, //state등록완료
+  },
 });
