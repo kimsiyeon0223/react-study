cfd8040cc8cb1ee44162b8410a5bc447a53213de
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-16 18:41:35 +0900

    cart state 만들기

--- shop/src/store.js
+++ shop/src/store.js
@@ -12,12 +12,20 @@ let stock = createSlice({
   initialState: [10, 11, 12],
 });
 
+let cart = createSlice({
+  name: "cart",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
+});
+
 export default configureStore({
   reducer: {
     user: user.reducer, //state등록완료
     stock: stock.reducer,
+    cart: cart.reducer,
   },
 });
-
 
 //redux store 안에 모든걸 넣진 말자
